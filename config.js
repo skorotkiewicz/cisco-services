ba2a593b12b3051590876ad5e8528cda687e9fdf
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2025-02-25 12:47:47 +0100

    chenge url

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const urlServer = "http://192.168.0.124:3000";
 export const port = 3000;
+export const urlServer = `http://192.168.0.124:${port}`;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cisco from "./cisco.js";
-import { port } from "./config.js";
+import { port, urlServer } from "./config.js";
 const app = express();
 
 app.use("/", cisco);
 
 app.listen(port, () => {
-  console.log(`Server running at http://0.0.0.0:${port}/`);
+  console.log(`Server running at ${urlServer}`);
 });

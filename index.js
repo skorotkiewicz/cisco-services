b0a5ef30b892580cc0f8d674e55f56ddefb369a8
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2025-03-12 18:42:31 +0100

    simplfy variable

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cisco from "./cisco.js";
-import directoryRouter from "./directory.js";
+import directory from "./directory.js";
 import { port, urlServer } from "./config.js";
 const app = express();
 
 app.use("/", cisco);
-app.use("/", directoryRouter);
+app.use("/", directory);
 
 app.listen(port, () => {
   console.log(`Server running at ${urlServer}`);
